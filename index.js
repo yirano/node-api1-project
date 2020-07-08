ff112b53dbd73955d7a7231a0b7b184643ccb836
yirano <yirano@protonmail.ch>
2020-07-08 02:10:37 -0400

    Allow user to edit and delete

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const PORT = 8080
 // get all users
 server.get("/api/users", async (req, res) => {
   const users = await db.find()
-  users ?
+  if (users) {
     res.json(users)
-    : res.status(500).json({
+  } else {
+    res.status(500).json({
       message: 'The user information could not be retrieved.',
     })
+  }
 })
 
 // get user by ID
@@ -32,13 +34,50 @@ server.get('/api/users/:id', async (req, res) => {
 })
 
 // create a user
-// server.post()
+server.post('/api/users', async (req, res) => {
+  if (req.body.name && req.body.bio) {
+    const newUser = db.insert({
+      name: req.body.name,
+      bio: req.body.bio,
+    })
+    res.json(await db.find())
+  } else {
+    res.json({
+      message: "Could not post user"
+    })
+  }
+})
 
 // edit a user by id
-// server.put()
+server.put('/api/users/:id', async (req, res) => {
+  const user = await db.findById(req.params.id)
+  if (user) {
+    const updated = db.update(user.id, {
+      name: req.body.name,
+      bio: req.body.bio
+    })
+    if (await updated) {
+      res.json(await db.find())
+    }
+  } else {
+    res.json({
+      message: 'Could not update user'
+    })
+  }
+})
 
 // delete a user by id
-// server.delete()
+server.delete('/api/users/:id', async (req, res) => {
+  const user = await db.findById(req.params.id)
+  if (user) {
+    const deleted = db.remove(user.id)
+    res.json(await deleted)
+  } else {
+    res.json({
+      message: 'User not found'
+    })
+  }
+})
 
 server.listen(8080, () => {
   console.log(`Server started on ${PORT}`)
