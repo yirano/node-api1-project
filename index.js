e3fe582f7a69f94d84f408b1a4562dfcfaa9100b
yirano <yirano@protonmail.ch>
2020-07-08 00:40:20 -0400

    Init to win it - set up index.js - import express - initialize server - set up PORT - set up for HTTP requests

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+const express = require('express')
+
+const server = express()
+
+const PORT = 8080
+
+// get all users
+server.get()
+
+// get user by ID
+server.get()
+
+// create a user
+server.post()
+
+// edit a user by id
+server.put()
+
+// delete a user by id
+server.delete()
+
+server.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`)
+})
